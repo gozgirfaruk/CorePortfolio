c4c7f657a16d0882b89994b247e11e1eec3bb2cb
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2024-03-20 01:28:25 +0300

    ...

--- Portfolio/Controllers/SkillController.cs
+++ Portfolio/Controllers/SkillController.cs
@@ -10,9 +10,6 @@ namespace Portfolio.UI.Controllers
         SkillMenager m_SkillMenager = new SkillMenager(new EfSkillDal());
         public IActionResult Index()
         {
-            ViewBag.a = "Yetenek Listesi";
-            ViewBag.b = "Yetenekler";
-            ViewBag.c = "Yetenek Listesi";
             var values = m_SkillMenager.GetList();
             return View(values);
         }
@@ -20,15 +17,35 @@ namespace Portfolio.UI.Controllers
         [HttpGet]
         public IActionResult AddSkill()
         {
-            ViewBag.a = "Yetenek Ekleme";
-            ViewBag.b = "Yetenekler";
-            ViewBag.c = "Yetenek Ekleme";
             return View();
         }
         [HttpPost]
         public IActionResult AddSkill(Skill skill)
         {
+            skill.SkillImage = "#";
             m_SkillMenager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteSkill(int id)
+        {
+            var values = m_SkillMenager.GetById(id);
+            m_SkillMenager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditSkill(int id)
+        {
+            var values = m_SkillMenager.GetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditSkill(Skill skill)
+        {
+            skill.SkillImage = "#";
+            m_SkillMenager.TUpdate(skill);
             return RedirectToAction("Index");
         }
     }
