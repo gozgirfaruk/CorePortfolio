ae5e73b89ce625599a6ae7d4fd77d5aab684e0bc
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2024-04-14 21:21:41 +0300

    Ajax added for WriterUser

---
+++ Portfolio.BusinessLayer/Concrete/DenemeMenager.cs
@@ -0,0 +1,46 @@
+﻿using Portfolio.Business.Abstract;
+using Portfolio.Entity.Concrete;
+using Potfolio.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Business.Concrete
+{
+    public class DenemeMenager : IDenemeService
+    {
+        IDenemeDal _denemeDal;
+
+        public DenemeMenager(IDenemeDal denemeDal)
+        {
+            _denemeDal = denemeDal;
+        }
+
+        public WriterUser GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<WriterUser> GetList()
+        {
+            return _denemeDal.GetList();
+        }
+
+        public void TAdd(WriterUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(WriterUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(WriterUser entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Portfolio/Controllers/WriterUserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Portfolio.Business.Concrete;
+using Portfolio.Entity.Concrete;
+using Potfolio.DataAccess.EntityFramework;
+
+namespace Portfolio.UI.Controllers
+{
+    public class WriterUserController : Controller
+    { 
+        DenemeMenager menager = new DenemeMenager(new EfDenemeDal());
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public IActionResult ListUser()
+        {
+            var values = JsonConvert.SerializeObject(menager.GetList());
+            return View(values);    
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(WriterUser p)
+        {
+            menager.TAdd(p);
+            var values = JsonConvert.SerializeObject(p);
+            return Json(values);
+
+        }
+    }
+}
