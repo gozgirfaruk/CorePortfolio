9efa531b13273f5ca21e71979178cbed1a718039
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2024-04-15 13:41:49 +0300

    completed

---
+++ Portfolio/Controllers/Experience2Controller.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Portfolio.Business.Concrete;
+using Portfolio.Entity.Concrete;
+using Potfolio.DataAccess.EntityFramework;
+
+namespace Portfolio.UI.Controllers
+{
+    public class Experience2Controller : Controller
+    {
+        ExperienceMenager menager = new ExperienceMenager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListExperience()
+        {
+            var values = JsonConvert.SerializeObject(menager.GetList());
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddExperience(Experience p)
+        {
+            menager.TAdd(p);
+            var values = JsonConvert.SerializeObject(p);
+            return Json(values);
+
+        }
+
+        public IActionResult GetByID(int id)
+        {
+            var values = menager.GetById(id);
+            var val =JsonConvert.SerializeObject(values);
+            return View(val);
+        }
+
+    }
+}
